8d1bed361e26597ea9837d20ac6f42de6ce89273
Rohitash Kator <rohitashkator@Rohitashs-MacBook-Pro.local>
2023-12-28 20:14:13 +0530

    initial commit

--- src/components/LessonPlan.tsx
+++ src/components/LessonPlan.tsx
@@ -1,7 +1,18 @@
+import { ChangeEvent, useRef } from "react";
 import styles from "./General.module.css";
 import { FiChevronLeft } from "react-icons/fi";
 
 const LessonPlan = () => {
+  let fileInputRef = useRef<HTMLInputElement>(null);
+
+  const uploadClickHandler = () => {
+    fileInputRef.current?.click();
+  };
+  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const uploadedFile = e.target.files?.[0];
+    console.log(uploadedFile);
+  };
+
   return (
     <div className={styles.content}>
       <p className={styles.titleText}>
@@ -12,7 +23,19 @@ const LessonPlan = () => {
         <input className={styles.inputField} placeholder="Insert URL here" />
         <p>Or</p>
       </div>
-      <div className={styles.uploadBox}>Upload</div>
+      <button
+        type="button"
+        className={styles.uploadBox}
+        onClick={uploadClickHandler}
+      >
+        Upload
+      </button>
+      <input
+        ref={fileInputRef}
+        style={{ display: "none" }}
+        type="file"
+        onChange={handleFileChange}
+      />
     </div>
   );
 };
